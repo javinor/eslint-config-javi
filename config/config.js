f3d11a67ec58f56f46bae178f8f90fa1a3376a9c
javinor <javinor@gmail.com>
2016-12-03 19:36:43 +0200

    split eslintrc into seperate files and folders

---
+++ config/config.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = {
+  extends: [
+    '../env/env.js',
+    '../rules/rules.js'
+  ]
+}
---
+++ env/env.js
@@ -0,0 +1,18 @@
+'use strict'
+
+module.exports = {
+  'parserOptions': {
+    'ecmaVersion': 7,
+    'ecmaFeatures': {
+      'experimentalObjectRestSpread': true
+    }
+  },
+  'env': {
+    'es6': true,
+    'node': true
+  },
+  'plugins': [],
+  'globals': {
+    'console': false
+  }
+}
