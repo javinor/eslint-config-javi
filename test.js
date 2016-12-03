cec6d92b0b6a19b9456a60ff10020bbd7b44f642
javinor <javinor@gmail.com>
2016-12-03 19:53:53 +0200

    remove dependecy version check

--- test.js
+++ test.js
@@ -2,11 +2,6 @@
 
 const test = require('ava')
 const { CLIEngine } = require('eslint')
-const { devDependencies, peerDependencies } = require('./package.json')
-
-test('eslint\'s devDependency and peerDependency match', t => {
-  t.is(`>=${devDependencies.eslint}`, peerDependencies.eslint)
-})
 
 test('config loads and is executed on current directory', t => {
   const cli = new CLIEngine()
