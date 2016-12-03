a6e926e59b2242bbbc2879939cb3319a37b25af8
javinor <javinor@gmail.com>
2016-12-03 19:46:04 +0200

    use ava and remove tape, faucet

--- test.js
+++ test.js
@@ -1,19 +1,17 @@
 'use strict'
 
-const test = require('tape')
+const test = require('ava')
 const { CLIEngine } = require('eslint')
 const { devDependencies, peerDependencies } = require('./package.json')
 
 test('eslint\'s devDependency and peerDependency match', t => {
   t.is(`>=${devDependencies.eslint}`, peerDependencies.eslint)
-  t.end()
 })
 
 test('config loads and is executed on current directory', t => {
   const cli = new CLIEngine()
   const { errorCount, results: { length } } = cli.executeOnFiles(['.'])
 
-  t.is(length, 2)
+  t.is(length, 5)
   t.is(errorCount, 0)
-  t.end()
 })
